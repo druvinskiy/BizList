054454334b751747d2809b760b0812525fc37ab1
DavidRuvinskiy <david.ruvinskiy@sbcglobal.net>
2017-06-28 14:57:05 -0500

    Initial Commit

---
+++ BizList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BizList
+//
+//  Created by David Ruvinskiy on 6/28/17.
+//  Copyright © 2017 David Ruvinskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
